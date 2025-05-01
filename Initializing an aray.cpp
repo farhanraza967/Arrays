3288cc7ff43adbada809d59f6914dbd7274b835f
Farhan Raza <razafarhan967@gmail.com>
2025-05-01 15:50:18 +0500

    Create Initializing an aray.cpp

---
+++ Initializing an aray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define LIM 3 
+int main(void) 
+{ 
+float num[LIM]={12.9, 9.0, 986.89}; 
+char ch[]={'a','b','c','d'};        
+                                                                          
+cout<<"float array:"<<num[0]<<","<<num[1]<<","<<num[2]<<endl; 
+cout<<"char array:"<<ch[0] <<","<<ch[1] <<","<<ch[2] <<","<<ch[3]; 
+return 0; 
+} 
