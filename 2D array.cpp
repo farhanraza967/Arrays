452e7833372660edcfc2d00ba805b15bd51c8b65
Farhan Raza <razafarhan967@gmail.com>
2025-05-02 20:16:36 +0500

    Create 2D array.cpp

---
+++ 2D array.cpp
@@ -0,0 +1,29 @@
+#include<iostream> 
+using namespace std; 
+#define row 5 
+#define col 3 
+int main(void) 
+{ 
+ int i,j; 
+    float num[row][col]; 
+ cout<<"Enter elements of array:"; 
+    for(i=0;i<row;i++)  
+    { 
+        for(j=0;j<col;j++) 
+        { 
+      cout<<"\nEnter location "<<i<<","<<j<<":"; 
+   cin>>num[i][j]; 
+   } 
+  } 
+  cout<<"\n \n"; 
+  for(i=0;i<row;i++)  // loop for printing 
+ { 
+  for(j=0;j<col;j++) 
+  { 
+   cout<<"Location "<<i<<","<<j<<"=" 
+   <<num[i][j]<<"\t"; 
+  } 
+  cout<<"\n"; 
+ } 
+    return 0; 
+} 
