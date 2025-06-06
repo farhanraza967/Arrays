02a7bda12593e17c12c3d081246fe0c635471c4a
Farhan Raza <razafarhan967@gmail.com>
2025-06-06 16:31:31 +0500

    Update Elements list.cpp

--- Elements list.cpp
+++ Elements list.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-  
 string friends[] = {"Alan", "Bob", "Carol", "David", "Ellen", "Fred", "Grace", "Henry", "Ian", "Jen"};
 for (int i = 0; i < sizeof(friends) / sizeof(friends[0]); i++) {
   cout << "Element at " <<i<<" is "<< friends[i] << endl;
