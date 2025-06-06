97fdfc304f9cb97cdf0a30959deabc7ae64979ba
Farhan Raza <razafarhan967@gmail.com>
2025-06-06 16:57:19 +0500

    Update enhanced loop.cpp

--- enhanced loop.cpp
+++ enhanced loop.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-  
-  //add code below this line
-
-string friends[] = {"Alan", "Bob", "Carol", "David", "Ellen", 
-                    "Fred", "Grace", "Henry", "Ian", "Jen"};
-
-for (string i : friends) {
+int main(){
+  // use to print all values of an array in one loop
+string friends[] = {"Alan", "Bob", "Carol", "David", "Ellen", "Fred", "Grace", "Henry", "Ian", "Jen"};
+for (string i : friends){
   cout << "he is " <<i << endl;
 }
-
-  //add code above this line
-  
   return 0;
-  
 }
