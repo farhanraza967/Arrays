5c34064c24cdf85800f353f7915621d300a729a1
Farhan Raza <razafarhan967@gmail.com>
2025-06-07 19:03:08 +0500

    Update Loops in array.cpp

--- Loops in array.cpp
+++ Loops in array.cpp
@@ -1,17 +1,13 @@
 #include<iostream> 
 using namespace std; 
- 
 #define LIM 10 
- 
 int main(){ 
     float num[LIM]; 
     int index_count;
-    for(index_count=0;index_count<LIM;index_count++) 
-    { 
+    for(index_count=0;index_count<LIM;index_count++){ 
         num[index_count]=(index_count+1)*2; 
     }  
-    for(index_count=0;index_count<LIM;index_count++) 
-    { 
+    for(index_count=0;index_count<LIM;index_count++){ 
         cout<<"\nElement no."<<index_count<<" = "<<num[index_count]; 
     }  
     return 0; 
