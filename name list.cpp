bd5cb495c81aee5e3ed4f2a3ae1662c07bcee033
Farhan Raza <razafarhan967@gmail.com>
2025-06-11 12:35:59 +0500

    Update name list.cpp

--- name list.cpp
+++ name list.cpp
@@ -2,14 +2,11 @@
 using namespace std;
 #define row 10
 #define col 25
-
-int main() {
+int main(){
     char names[row][col] = {}; 
     int i = 0, j = 0;
     char ch;
-
     cout << "Enter name of 10 students, press ESC to stop\n";
-
     while (i < row) {
         j = 0;
         cout << "\nEnter name " << i + 1 << ": ";
@@ -39,8 +36,6 @@ int main() {
 
         cout << "Press ESC to stop, ENTER to continue...\n";
     }
-
-print_names:
     cout << "\n\nEntered Names:\n";
     for (int k = 0; k < i; ++k) {
         cout << k + 1 << ". ";
